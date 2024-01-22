00cb57b04d51dc70c65f96f4df03219841aa313b
Kama-Verga <132174425+Kama-Verga@users.noreply.github.com>
2024-01-22 18:29:43 +0100

    Progetto inizializzato da Spring Inizilizer

---
+++ Progetto/src/main/java/ProgettoIngegneria/Progetto/ProgettoDiIngegneriaApplication.java
@@ -0,0 +1,13 @@
+package ProgettoIngegneria.Progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoDiIngegneriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoDiIngegneriaApplication.class, args);
+	}
+
+}
---
+++ Progetto/src/test/java/ProgettoIngegneria/Progetto/ProgettoDiIngegneriaApplicationTests.java
@@ -0,0 +1,13 @@
+package ProgettoIngegneria.Progetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoDiIngegneriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
