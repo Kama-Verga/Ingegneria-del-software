fc9a1d7ce0b1be7c632ff1f1d7a1a33239e86068
Kama-Verga <132174425+Kama-Verga@users.noreply.github.com>
2024-01-22 13:21:44 +0100

    Base generata da Spring Boot
    Grade- grovy
    Java 1.7
    3.2.2

---
+++ ProgettoIngegneria/src/main/java/progettoIngegneria/ProgettoIngegneria/ProgettoIngegneriaApplication.java
@@ -0,0 +1,13 @@
+package progettoIngegneria.ProgettoIngegneria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoIngegneriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoIngegneriaApplication.class, args);
+	}
+
+}
